5995e5092a80d754fe531d300c247aeda386dd1f
AqibUsman <aqibusman04@gmail.com>
2017-03-12 12:30:11 +0500

    JS Assignment 2

---
+++ app.js
@@ -0,0 +1,7 @@
+var a = +prompt("Enter your percentage");
+
+if (a >= 80) alert("Awesome! A+ grade");
+else if (a <= 79 && a >= 70) alert("Welldone You Got A grade");
+else if (a <= 69 && a >= 60) alert("Average B+");
+else if (a <= 59 && a >= 50) alert("Not Good C grade");
+else if (a <= 49) alert("Unbelievable You are fail");
